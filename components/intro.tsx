8cddf01df513ec19fde8e4eda94d57bccd079a63
daveprayag <prayagdave28@gmail.com>
2023-09-05 15:44:51 +0530

    Implemented Header Component

---
+++ components/intro.tsx
@@ -0,0 +1,55 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import PD1 from "@/public/PD1.jpg";
+import PD2 from "@/public/PD2.jpg";
+import { motion } from "framer-motion";
+
+export default function Intro() {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}
+          >
+            <Image
+              src={PD2}
+              alt="Prayag portrait"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] object-top border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="text-4xl absolute bottom-0 right-0"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋🏼
+          </motion.span>
+        </div>
+      </div>
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Prayag.</span> I'm a{" "}
+        <span className="font-bold">full-stack developer.</span> I enjoy
+        building sites & apps. My focus is{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.h1>
+    </section>
+  );
+}
