7cb968641bea0f86cd45991f338388ea237f2d80
daveprayag <prayagdave28@gmail.com>
2023-09-29 22:54:36 +0530

    Analytics bug fix v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Analytics />
           <Footer />
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
