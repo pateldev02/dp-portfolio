11ce14aeb1def5bf910014441aa6802b0028be2c
daveprayag <prayagdave28@gmail.com>
2023-11-13 16:45:36 +0530

    Fixed header active experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -11,7 +11,7 @@ import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function Experience() {
-  const { ref } = useSectionInView("Experience");
+  const { ref } = useSectionInView("Experience", 0.5);
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My Journey</SectionHeading>
